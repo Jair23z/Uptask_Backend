f424868d733172862982850a1cf3e5be37a39079
Jair23z <jair121sp@gmail.com>
2025-09-14 18:51:25 -0600

    Segundo

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,18 +1,18 @@
-import { CorsOptions } from 'cors'
+import { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
+        const whiteList = [process.env.FRONTEND_URL];
 
-        const whitelist = [process.env.FRONTEND_URL]
-
-        if (process.argv[2] === '--api') {
-            whitelist.push(undefined)
+        if (process.argv[2] == '--api') {
+            whiteList.push(undefined);
         }
-
-        if (whitelist.includes(origin)) {
-            callback(null, true)
+        if (whiteList.includes(origin)) {
+            callback(null, true);
         } else {
-            callback(new Error('Error de Cors'))
+            callback(new Error('Error de CORS'));
         }
-    }
-}
+    },
+};
